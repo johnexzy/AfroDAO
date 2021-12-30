9ac186cf54053cedf18701b02b7e9183444b8070
Johnexzy <obajohn75@gmail.com>
2021-12-30 08:37:57 +0100

    Refactor

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,9 @@ import { useWeb3 } from "@3rdweb/hooks";
 // We instantiate the sdk on Rinkeby.
 const sdk = new ThirdwebSDK("rinkeby");
 
-
 // We can grab a reference to our ERC-1155 contract.
 const bundleDropModule = sdk.getBundleDropModule(
-  "0x593ae01AA255a5Cb080F452d5a5Aaf4f1350dE24",
+  "0x593ae01AA255a5Cb080F452d5a5Aaf4f1350dE24"
 );
 
 const tokenModule = sdk.getTokenModule(
@@ -24,14 +23,15 @@ const App = () => {
   // The array holding all of our members addresses.
   const [memberAddresses, setMemberAddresses] = useState([]);
 
-  // A fancy function to shorten someones wallet address, no need to show the whole thing. 
+  // A fancy function to shorten someones wallet address, no need to show the whole thing.
   const shortenAddress = (str) => {
     return str.substring(0, 6) + "..." + str.substring(str.length - 4);
   };
 
   // Use the connectWallet hook thirdweb gives us.
-  const { connectWallet, address, error, provider, disconnectWallet } = useWeb3();
-  console.log("👋 Address:", address)
+  const { connectWallet, address, error, provider, disconnectWallet } =
+    useWeb3();
+  console.log("👋 Address:", address);
   // The signer is required to sign transactions on the blockchain.
   // Without it we can only read data, not write.
   const signer = provider ? provider.getSigner() : undefined;
@@ -40,88 +40,89 @@ const App = () => {
   // isClaiming lets us easily keep a loading state while the NFT is minting.
   const [isClaiming, setIsClaiming] = useState(false);
 
-const HasClaimedNFT = () => {
-  return (
-    <div className="member-page">
-      <h1>AfroDAO Member Page</h1>
-      <p>Congratulations on being a member</p>
-      <div>
+  const HasClaimedNFT = () => {
+    return (
+      <div className="member-page">
+        <DisconnectWallet />
+        <h1>AfroDAO Member Page</h1>
+        <p>Congratulations on being a member</p>
         <div>
-          <h2>Member List</h2>
-          <table className="card">
-            <thead>
-              <tr>
-                <th>Address</th>
-                <th>Token Amount</th>
-              </tr>
-            </thead>
-            <tbody>
-              {memberList.map((member) => {
-                return (
-                  <tr key={member.address}>
-                    <td>{shortenAddress(member.address)}</td>
-                    <td>{member.tokenAmount}</td>
-                  </tr>
-                );
-              })}
-            </tbody>
-          </table>
+          <div>
+            <h2>Member List</h2>
+            <table className="card">
+              <thead>
+                <tr>
+                  <th>Address</th>
+                  <th>Token Amount</th>
+                </tr>
+              </thead>
+              <tbody>
+                {memberList.map((member) => {
+                  return (
+                    <tr key={member.address}>
+                      <td>{shortenAddress(member.address)}</td>
+                      <td>{member.tokenAmount}</td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
-    </div>
-  );
-}
-// This useEffect grabs all the addresses of our members holding our NFT.
-useEffect(() => {
-  if (!hasClaimedNFT) {
-    return (<HasClaimedNFT />);
-  }
-  
-  // Just like we did in the 7-airdrop-token.js file! Grab the users who hold our NFT
-  // with tokenId 0.
-  bundleDropModule
-    .getAllClaimerAddresses("0")
-    .then((addresess) => {
-      console.log("🚀 Members addresses", addresess)
-      setMemberAddresses(addresess);
-    })
-    .catch((err) => {
-      console.error("failed to get member list", err);
+    );
+  };
+  // This useEffect grabs all the addresses of our members holding our NFT.
+  useEffect(() => {
+    if (!hasClaimedNFT) {
+      return ;
+    }
+
+    // Just like we did in the 7-airdrop-token.js file! Grab the users who hold our NFT
+    // with tokenId 0.
+    bundleDropModule
+      .getAllClaimerAddresses("0")
+      .then((addresses) => {
+        console.log("🚀 Members addresses", addresses);
+        setMemberAddresses(addresses);
+      })
+      .catch((err) => {
+        console.error("failed to get member list", err);
+      });
+  }, [hasClaimedNFT]);
+
+  // This useEffect grabs the # of token each member holds.
+  useEffect(() => {
+    if (!hasClaimedNFT) {
+      return;
+    }
+
+    // Grab all the balances.
+    tokenModule
+      .getAllHolderBalances()
+      .then((amounts) => {
+        console.log("👜 Amounts", amounts);
+        setMemberTokenAmounts(amounts);
+      })
+      .catch((err) => {
+        console.error("failed to get token amounts", err);
+      });
+  }, [hasClaimedNFT]);
+
+  // Now, we combine the memberAddresses and memberTokenAmounts into a single array
+  const memberList = useMemo(() => {
+    return memberAddresses.map((address) => {
+      return {
+        address,
+        tokenAmount: ethers.utils.formatUnits(
+          // If the address isn't in memberTokenAmounts, it means they don't
+          // hold any of our token.
+          memberTokenAmounts[address] || 0,
+          18
+        ),
+      };
     });
-}, [hasClaimedNFT]);
-
-// This useEffect grabs the # of token each member holds.
-useEffect(() => {
-  if (!hasClaimedNFT) {
-    return;
-  }
-
-  // Grab all the balances.
-  tokenModule
-    .getAllHolderBalances()
-    .then((amounts) => {
-      console.log("👜 Amounts", amounts)
-      setMemberTokenAmounts(amounts);
-    })
-    .catch((err) => {
-      console.error("failed to get token amounts", err);
-    });
-}, [hasClaimedNFT]);
-
-// Now, we combine the memberAddresses and memberTokenAmounts into a single array
-const memberList = useMemo(() => {
-  return memberAddresses.map((address) => {
-    return {
-      address,
-      tokenAmount: ethers.utils.formatUnits(
-        // If the address isn't in memberTokenAmounts, it means they don't
-        // hold any of our token.
-        memberTokenAmounts[address] || 0,
-        18,
-      ),
-    };
-  });
-}, [memberAddresses, memberTokenAmounts]);
+  }, [memberAddresses, memberTokenAmounts]);
   // Another useEffect!
   useEffect(() => {
     // We pass the signer to the sdk, which enables us to interact with
@@ -141,10 +142,10 @@ const memberList = useMemo(() => {
         // If balance is greater than 0, they have our NFT!
         if (balance.gt(0)) {
           setHasClaimedNFT(true);
-          console.log("🌟 this user has a membership NFT!")
+          console.log("🌟 this user has a membership NFT!");
         } else {
           setHasClaimedNFT(false);
-          console.log("😭 this user doesn't have a membership NFT.")
+          console.log("😭 this user doesn't have a membership NFT.");
         }
       })
       .catch((error) => {
@@ -161,28 +162,7 @@ const memberList = useMemo(() => {
           Disconnect
         </button>
       </div>
-    )
-  }
-  if (!address) {
-    return (
-      <div className="landing">
-        <h1>Welcome to AfroDAO</h1>
-        <button onClick={() => connectWallet("injected")} className="btn-hero">
-          Connect your Metamask
-        </button>
-        <button onClick={() => connectWallet("walletconnect")} className="btn-hero margin" >
-          Connect your wallet with walletconnect
-        </button>
-
-        <button onClick={() => connectWallet("walletlink")} className="btn-hero margin" >
-          Connect your coinbase wallet
-        </button>
-      </div>
     );
-  }
-  // Add this little piece!
-  if (hasClaimedNFT) {
-     return (<HasClaimedNFT />);
   };
   // This is the case where we have the user's address
   const mintNft = () => {
@@ -205,21 +185,45 @@ const memberList = useMemo(() => {
         // Stop loading state.
         setIsClaiming(false);
       });
-  }
+  };
+  if (!address) {
+    return (
+      <div className="landing">
+        <h1>Welcome to AfroDAO</h1>
+        <button onClick={() => connectWallet("injected")} className="btn-hero">
+          Connect your Metamask
+        </button>
+        <button
+          onClick={() => connectWallet("walletconnect")}
+          className="btn-hero margin"
+        >
+          Connect your wallet with walletconnect
+        </button>
 
-  // Render mint nft screen.
-  return (
-    <div className="mint-nft">
-      <h1>Mint your free 🍪DAO Membership NFT</h1>
-      <button
-        disabled={isClaiming}
-        onClick={() => mintNft()}
-      >
-        {isClaiming ? "Minting..." : "Mint your nft (FREE)"}
-      </button>
-      <DisconnectWallet />
-    </div>
-  );
+        <button
+          onClick={() => connectWallet("walletlink")}
+          className="btn-hero margin"
+        >
+          Connect your coinbase wallet
+        </button>
+      </div>
+    );
+  } else {
+    // Add this little piece!
+    if (hasClaimedNFT) {
+      return (<HasClaimedNFT />);
+    }
+    // Render mint nft screen.
+    return (
+      <div className="mint-nft">
+        <h1>Mint your free 🍪DAO Membership NFT</h1>
+        <button disabled={isClaiming} onClick={() => mintNft()}>
+          {isClaiming ? "Minting..." : "Mint your nft (FREE)"}
+        </button>
+        <DisconnectWallet />
+      </div>
+    );
+  }
 };
 
 export default App;
